8a6720abb6079474a09064558b92558ebbe1f148
Demdelf <noignor@mail.ru>
2019-11-08 18:00:49 +0300

    Initial commit

---
+++ src/main/java/testgroup/filmography/controller/FilmController.java
@@ -0,0 +1,103 @@
+package testgroup.filmography.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import testgroup.filmography.model.Film;
+import testgroup.filmography.service.FilmService;
+import testgroup.filmography.service.FilmServiceImpl;
+
+import java.util.List;
+
+@Controller
+public class FilmController {
+    private FilmService filmService/* = new FilmServiceImpl()*/;
+    private int page;
+
+    @Autowired
+    public void setFilmService(FilmService filmService) {
+        this.filmService = filmService;
+    }
+    //private static Film film;
+
+  /*  static {
+        film = new Film();
+        film.setTitle("Inception");
+        film.setYear(2010);
+        film.setGenre("sci-fi");
+        film.setWatched(true);
+    }*/
+
+   /* @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView allFilms() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("films");
+        modelAndView.addObject("film", films);
+        return modelAndView;
+    }*/
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView allFilms(@RequestParam(defaultValue = "1") int page) {
+        this.page = page;
+        List<Film> films = filmService.allFilms(page);
+        int filmsCount = filmService.filmsCount();
+        int pagesCount = (filmsCount + 9)/10;
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("films");
+        modelAndView.addObject("page", page);
+        modelAndView.addObject("filmsList", films);
+        modelAndView.addObject("filmsCount", filmsCount);
+        modelAndView.addObject("pagesCount", pagesCount);
+        return modelAndView;
+    }
+
+    /*@RequestMapping(value = "/edit", method = RequestMethod.GET)
+    public ModelAndView editPage() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("editPage");
+        return modelAndView;
+    }*/
+
+    @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+    public ModelAndView editPage(@PathVariable("id") int id) {
+        Film film = filmService.getById(id);
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("editPage");
+        modelAndView.addObject("film", film);
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/edit", method = RequestMethod.POST)
+    public ModelAndView editFilm(@ModelAttribute("film") Film film) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("redirect:/?page=" + this.page);
+        filmService.edit(film);
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public ModelAndView addPage() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("editPage");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public ModelAndView addFilm(@ModelAttribute("film") Film film) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("redirect:/?page=" + this.page);
+        filmService.add(film);
+        return modelAndView;
+    }
+
+    @RequestMapping(value="/delete/{id}", method = RequestMethod.GET)
+    public ModelAndView deleteFilm(@PathVariable("id") int id) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("redirect:/?page=" + this.page);
+        Film film = filmService.getById(id);
+        filmService.delete(film);
+        return modelAndView;
+    }
+
+}
